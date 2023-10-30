8ca7c4b318af181682cd56dd5fa883f3287c4436
Carlevaro <m.carlevaro@gmail.com>
2023-10-30 20:35:02 -0300

    u12 segunda clase

---
+++ 12-ecuaciones_segundo_orden/code/parabolica-regresiva.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import math
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.rcParams.update({"text.usetex": True})
+fig, ax = plt.subplots(figsize=(8,6))
+import numpy as np
+
+def sol_exacta(x, t):
+    u = math.exp(-np.pi**2 * t) * np.sin(np.pi * x)
+    return u
+
+def solve_matricial(u, h, k, alfa):
+    lamb = alfa**2 * k / h**2
+    n = u.shape[0]
+    A = np.diagflat(np.ones(n) * (1 + 2 * lamb)) \
+        + np.diagflat(np.ones(n - 1) * (-lamb), -1) \
+        + np.diagflat(np.ones(n - 1) * (-lamb), 1)
+    n_t = int(0.5 / k)
+    print(A)
+    print(u)
+    for i in range(n_t):
+        u = np.linalg.solve(A, u)
+        u[0] = u[-1] = 0.0
+    return u
+
+n = 10
+h = 0.1
+# k = 0.0005
+k = 0.01
+alfa = 1.0
+x = np.arange(n + 1) * h
+u_0 = np.sin(np.pi * x)
+u = solve_matricial(u_0, h, k, alfa)
+u_ex = sol_exacta(x, 0.5)
+for i in range(x.size):
+    # print(f"{i:1d} & {x[i]:.1f} & {u[i]:.3e} & {u_ex[i]:.5f} & {abs(u[i] - u_ex[i]):.3e} \\\\")
+    print(f"{i:1d} & {x[i]:.1f} & {u[i]:.5f} & {u_ex[i]:.5f} & {abs(u[i] - u_ex[i]):.3e} \\\\")
+
+# plt.plot(x, u_0, label=r"$u(t = 0)$")
+plt.plot(x, u, label=r"$u(t = 0.5)$")
+plt.plot(x, u_ex, label=r"$u_e(t = 0.5)$")
+plt.xlabel(r"$x$")
+plt.ylabel(r"$T(x)$")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
