0f8fa14daaeb817bb6e6728f6c48ad6de1c1b5ab
Manux <manux.ch@gmail.com>
2024-05-23 16:39:29 -0300

    cierre 2024¨

--- 7-autovalores-autovectores/code/autoval-qr.py
+++ 7-autovalores-autovectores/code/autoval-qr.py
@@ -4,7 +4,7 @@ import numpy as np
 def algoritmo_qr(A, num_iter):
     n = A.shape[0]
     autovalores = np.zeros(n, dtype=np.complex128)
-    for _ in range(num_iter):
+    for i in range(num_iter):
         Q, R = np.linalg.qr(A)
         A = np.dot(R, Q)
     for i in range(n):
