ebc7f0723b3af90add371e783d162c2d03bd4ace
Manux <manux.ch@gmail.com>
2024-06-06 13:35:26 -0300

    tema 8 update

---
+++ 8-sistemas-lineales/code/gauss-seidel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+def gauss_seidel(A, b, tolerance=1e-10, max_iterations=10000):
+
+    x = np.zeros_like(b, dtype=np.double)
+
+    #Iterate
+    for k in range(max_iterations):
+        x_old  = x.copy()
+        #Loop over rows
+        for i in range(A.shape[0]):
+            x[i] = (b[i] - np.dot(A[i,:i], x[:i]) - np.dot(A[i,(i+1):], x_old[(i+1):])) / A[i ,i]
+
+        #Stop condition
+        if np.linalg.norm(x - x_old, ord=np.inf) / np.linalg.norm(x, ord=np.inf) < tolerance:
+            break
+
+    return x
+
+a = np.array([[3, 2, 0], [1, -1, 0], [0, 5, 1]])
+b = np.array([2, 4, -1])
+x = gauss_seidel(a, b)
+print(x)
+
